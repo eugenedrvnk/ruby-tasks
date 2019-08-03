ec7a857aee6a86c0272063a35e0016ae7bac2691
Eugene Derevyanko <eugenedrvnk@gmail.com>
2019-08-03 20:35:30 +0300

    fix euler-3

--- euler/3.rb
+++ euler/3.rb
@@ -1,22 +1,15 @@
+require 'Prime'
+
 def largest_prime_factor num
     num.to_f 
     factors = []
     (num/2).downto(2) do |i|
         temp = num/i.to_f
-        if temp % 1 == 0 && lower_factors(temp, factors) == false
+        if temp % 1 == 0 && Prime.prime?(temp.to_i)
             factors << temp.to_i
         end
     end
     p factors.max
 end
 
-def lower_factors(a, factors)
-    factors.each do |item|
-        if (a/item) % 1 == 0
-          return true  
-        end
-    end
-    return false
-end
-
-largest_prime_factor 600851475143
+largest_prime_factor 6000000
